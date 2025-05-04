738727897998fc7b9ed91bcbbf0123ff799b34c0
Lagz85 <jhewett@gmail.com>
2025-05-03 19:36:47 -0700

    df

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print('Bot placeholder')
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
